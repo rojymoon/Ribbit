ebbbb7d0c65f805542369bc8ee7b5a25c9195b20
rojymoon <raghda@users.noreply.github.com>
2014-10-01 20:42:16 +0300

    Added forgot password feature

---
+++ src/com/treehouselearning/ribbit/ForgotPasswordActivity.java
@@ -0,0 +1,90 @@
+package com.treehouselearning.ribbit;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.RequestPasswordResetCallback;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ForgotPasswordActivity extends Activity {
+
+	protected EditText mEmailRecovery;
+	protected Button mSubmitButton;
+	private static final String TAG = ForgotPasswordActivity.class
+			.getSimpleName();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_forgot_password);
+
+		mEmailRecovery = (EditText) findViewById(R.id.emailRecoveryField);
+		mSubmitButton = (Button) findViewById(R.id.submitButton);
+		mSubmitButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String emailRecovery = mEmailRecovery.getText().toString();
+				ParseUser.requestPasswordResetInBackground(emailRecovery,
+						new RequestPasswordResetCallback() {
+
+							@Override
+							public void done(ParseException e) {
+								if (e == null) {
+									// An email was successfully sent with reset
+									// instructions.
+									Toast.makeText(
+											ForgotPasswordActivity.this,
+											"An email was successfully sent with reset instructions.",
+											Toast.LENGTH_LONG).show();
+									Intent intent = new Intent(
+											ForgotPasswordActivity.this,
+											LoginActivity.class);
+									intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+									intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+									startActivity(intent);
+								} else {
+									// Something went wrong. Look at the
+									// ParseException to see what's up.
+									Log.e(TAG, e.getMessage());
+									Toast.makeText(
+											ForgotPasswordActivity.this,
+											e.getMessage(),
+											Toast.LENGTH_LONG).show();
+								}
+
+							}
+						});
+
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.forgot_password, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
--- src/com/treehouselearning/ribbit/LoginActivity.java
+++ src/com/treehouselearning/ribbit/LoginActivity.java
@@ -18,78 +18,112 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends Activity {
-	
+
 	protected EditText mUsername;
 	protected EditText mPassword;
 	protected Button mLoginButton;
+	protected Button mSignUpButton;
 	protected TextView mSignUpTextView;
+	protected TextView mForgotPassTextView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.activity_login);
-		
-		mSignUpTextView = (TextView) findViewById(R.id.signUpText);
+
+		/*mSignUpTextView = (TextView) findViewById(R.id.signUpText);
 		mSignUpTextView.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+				Intent intent = new Intent(LoginActivity.this,
+						SignUpActivity.class);
 				startActivity(intent);
-				
+
+			}
+		});*/
+		
+		mForgotPassTextView = (TextView) findViewById(R.id.forgotPasswordText);
+		mForgotPassTextView.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(LoginActivity.this,
+						ForgotPasswordActivity.class);
+				startActivity(intent);
+
 			}
 		});
-		
+
+		mSignUpButton = (Button) findViewById(R.id.signUpButton);
+		mSignUpButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(LoginActivity.this,
+						SignUpActivity.class);
+				startActivity(intent);
+
+			}
+		});
+
 		mUsername = (EditText) findViewById(R.id.usernameField);
-		mPassword = (EditText) findViewById(R.id.passwordField);		
-		
+		mPassword = (EditText) findViewById(R.id.passwordField);
+
 		mLoginButton = (Button) findViewById(R.id.loginButton);
 		mLoginButton.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				String username = mUsername.getText().toString();
 				String password = mPassword.getText().toString();
-				
+
 				username = username.trim();
 				password = password.trim();
-				
-				if(username.isEmpty() || password.isEmpty() ){
-					AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+
+				if (username.isEmpty() || password.isEmpty()) {
+					AlertDialog.Builder builder = new AlertDialog.Builder(
+							LoginActivity.this);
 					builder.setMessage(R.string.login_error_message)
-						.setTitle(R.string.signup_error_title)
-						.setPositiveButton(android.R.string.ok, null);
+							.setTitle(R.string.signup_error_title)
+							.setPositiveButton(android.R.string.ok, null);
 					AlertDialog dialog = builder.create();
 					dialog.show();
-				}
-				else{
-					//Login
+				} else {
+					// Login
 					setProgressBarIndeterminateVisibility(true);
-					ParseUser.logInInBackground(username, password, new LogInCallback() {						
-						@Override
-						public void done(ParseUser user, ParseException e) {
-							
-							setProgressBarIndeterminateVisibility(false);
-							//if(user != null){
-							if(e == null){
-								//Success! The user is logged in.
-								Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-								intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-								intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-								startActivity(intent);
-							}
-							else{
-								AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-								builder.setMessage(e.getMessage())
-									.setTitle(R.string.signup_error_title)
-									.setPositiveButton(android.R.string.ok, null);
-								AlertDialog dialog = builder.create();
-								dialog.show();
-							}
-							
-						}
-					}); 
+					ParseUser.logInInBackground(username, password,
+							new LogInCallback() {
+								@Override
+								public void done(ParseUser user,
+										ParseException e) {
+
+									setProgressBarIndeterminateVisibility(false);
+									// if(user != null){
+									if (e == null) {
+										// Success! The user is logged in.
+										Intent intent = new Intent(
+												LoginActivity.this,
+												MainActivity.class);
+										intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+										intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+										startActivity(intent);
+									} else {
+										AlertDialog.Builder builder = new AlertDialog.Builder(
+												LoginActivity.this);
+										builder.setMessage(e.getMessage())
+												.setTitle(
+														R.string.signup_error_title)
+												.setPositiveButton(
+														android.R.string.ok,
+														null);
+										AlertDialog dialog = builder.create();
+										dialog.show();
+									}
+
+								}
+							});
 				}
 			}
 		});
