9a1c6bf1ad0d8ace9405acb19db095c21b295f5c
rojymoon <raghda@users.noreply.github.com>
2014-10-18 01:06:10 +0300

    Completed stage 5

--- gen/com/treehouselearning/ribbit/R.java
+++ gen/com/treehouselearning/ribbit/R.java
@@ -8,6 +8,9 @@
 package com.treehouselearning.ribbit;
 
 public final class R {
+    public static final class array {
+        public static final int camera_choices=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -22,25 +25,27 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_camera=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_edit_friends=0x7f08000f;
-        public static final int action_logout=0x7f08000e;
-        public static final int action_settings=0x7f08000d;
-        public static final int buttonsLayout=0x7f080006;
-        public static final int emailField=0x7f08000c;
-        public static final int emailRecoveryField=0x7f080001;
-        public static final int enterEmailText=0x7f080000;
-        public static final int forgotPasswordText=0x7f080009;
-        public static final int loginButton=0x7f080007;
-        public static final int pager=0x7f08000a;
-        public static final int passwordField=0x7f080004;
-        public static final int signUpButton=0x7f080008;
-        public static final int signUpText=0x7f080005;
-        public static final int signupButton=0x7f08000b;
-        public static final int submitButton=0x7f080002;
-        public static final int usernameField=0x7f080003;
+        public static final int action_camera=0x7f090010;
+        public static final int action_edit_friends=0x7f09000e;
+        public static final int action_logout=0x7f09000f;
+        public static final int action_settings=0x7f09000d;
+        public static final int buttonsLayout=0x7f090006;
+        public static final int emailField=0x7f09000c;
+        public static final int emailRecoveryField=0x7f090001;
+        public static final int enterEmailText=0x7f090000;
+        public static final int forgotPasswordText=0x7f090009;
+        public static final int loginButton=0x7f090007;
+        public static final int pager=0x7f09000a;
+        public static final int passwordField=0x7f090004;
+        public static final int signUpButton=0x7f090008;
+        public static final int signUpText=0x7f090005;
+        public static final int signupButton=0x7f09000b;
+        public static final int submitButton=0x7f090002;
+        public static final int usernameField=0x7f090003;
     }
     public static final class layout {
         public static final int activity_edit_friends=0x7f030000;
@@ -52,10 +57,10 @@ public final class R {
         public static final int fragment_inbox=0x7f030006;
     }
     public static final class menu {
-        public static final int forgot_password=0x7f070000;
-        public static final int login=0x7f070001;
-        public static final int main=0x7f070002;
-        public static final int sign_up=0x7f070003;
+        public static final int forgot_password=0x7f080000;
+        public static final int login=0x7f080001;
+        public static final int main=0x7f080002;
+        public static final int sign_up=0x7f080003;
     }
     public static final class string {
         public static final int action_settings=0x7f050005;
@@ -64,11 +69,16 @@ public final class R {
         public static final int empty_friends_label=0x7f050019;
         public static final int empty_inbox_label=0x7f050018;
         public static final int enter_email_text=0x7f050016;
+        public static final int error_external_storage=0x7f05001e;
+        public static final int error_file_size_too_large=0x7f050022;
+        public static final int error_openeing_file=0x7f050021;
         public static final int error_title=0x7f05001c;
         public static final int forgot_password_text=0x7f050014;
+        public static final int general_error=0x7f05001f;
         public static final int hello_world=0x7f050007;
         public static final int login_button_label=0x7f05000a;
         public static final int login_error_message=0x7f050011;
+        public static final int menu_camera_label=0x7f05001d;
         public static final int menu_edit_friends_label=0x7f05001a;
         public static final int menu_logout_label=0x7f050012;
         public static final int parse_app_id=0x7f050000;
@@ -87,6 +97,7 @@ public final class R {
         public static final int title_section1=0x7f050003;
         public static final int title_section2=0x7f050004;
         public static final int username_hint=0x7f050008;
+        public static final int video_file_size_warning=0x7f050020;
     }
     public static final class style {
         /** 
@@ -110,10 +121,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/treehouselearning/ribbit/MainActivity.java
+++ src/com/treehouselearning/ribbit/MainActivity.java
@@ -1,9 +1,22 @@
 package com.treehouselearning.ribbit;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -11,13 +24,162 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.parse.ParseAnalytics;
 import com.parse.ParseUser;
 
-public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
-	
+public class MainActivity extends FragmentActivity implements
+		ActionBar.TabListener {
+
 	public static final String TAG = MainActivity.class.getSimpleName();
+
+	public static final int TAKE_PHOTO_REQUEST = 0;
+	public static final int TAKE_VIDEO_REQUEST = 1;
+	// public static final int PICK_PHOTO_REQUEST = 2;
+	// public static final int PICK_VIDEO_REQUEST = 3;
+	public static final int PICK_VIDEO_PHOTO_REQUEST = 2;
+	
+	public static boolean video_request_flag = false;
+
+	public static final int MEDIA_TYPE_IMAGE = 3;
+	public static final int MEDIA_TYPE_VIDEO = 4;
+	
+	public static final int FILE_SIZE_LIMIT = 1024*1024*10; //10 MB
+
+	protected Uri mMediaUri;
+
+	protected DialogInterface.OnClickListener mDialogListener = new DialogInterface.OnClickListener() {
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			switch (which) {
+			case 0: // Take picture
+				openPhotoIntent();
+				break;
+			case 1: // Take video
+				openVideoIntent();
+				break;
+			case 2: // Choose picture
+				/*
+				 * Intent choosePhotoIntent = new
+				 * Intent(Intent.ACTION_GET_CONTENT);
+				 * choosePhotoIntent.setType("image/*");
+				 * startActivityForResult(choosePhotoIntent,
+				 * PICK_PHOTO_REQUEST);
+				 */
+
+				pickPhotoOrVideoIntent("image/*");
+				break;
+			case 3: // Choose video
+				/*
+				 * Intent chooseVideoIntent = new
+				 * Intent(Intent.ACTION_GET_CONTENT);
+				 * chooseVideoIntent.setType("video/*");
+				 * startActivityForResult(chooseVideoIntent,
+				 * PICK_VIDEO_REQUEST);
+				 */
+				video_request_flag = true;
+				Toast.makeText(MainActivity.this, R.string.video_file_size_warning, Toast.LENGTH_LONG).show();
+				pickPhotoOrVideoIntent("video/*");
+
+				break;
+			}
+
+		}
+
+		private void pickPhotoOrVideoIntent(String type) {
+			Intent pickIntent = new Intent(Intent.ACTION_GET_CONTENT);
+			pickIntent.setType(type);
+			startActivityForResult(pickIntent, PICK_VIDEO_PHOTO_REQUEST);
+		}
+
+		private void openVideoIntent() {
+			Intent videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+			mMediaUri = getOutputMediaFileUri(MEDIA_TYPE_VIDEO);
+			if (mMediaUri == null) {
+
+			} else {
+				videoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+				videoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 10);
+				videoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0);
+				startActivityForResult(videoIntent, TAKE_VIDEO_REQUEST);
+			}
+		}
+
+		private void openPhotoIntent() {
+			Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			mMediaUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+			if (mMediaUri == null) {
+				// Display an error
+				Toast.makeText(MainActivity.this,
+						R.string.error_external_storage, Toast.LENGTH_LONG)
+						.show();
+			} else {
+				takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+				startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST);
+			}
+		}
+
+		private Uri getOutputMediaFileUri(int mediaType) {
+			// To be safe, you should check that the SDCard is mounted
+			// using Environment.getExternalStorageState() before doing this.
+			if (isExternalStorageAvailable()) {
+				// Get the URI
+
+				// 1. Get the external storage directory.
+				// String appName =
+				// MainActivity.this.getString(R.string.app_name);
+				File mediaStorageDir = new File(
+						Environment
+								.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+						getString(R.string.app_name));
+
+				// 2. Create our sub-directory.
+				if (!mediaStorageDir.exists()) {
+					if (!mediaStorageDir.mkdirs()) {
+						Log.e(TAG, "Failed to create directory.");
+						return null;
+					}
+				}
+				// 3. Create a filename.
+				// 4. Create the file.
+				File mediaFile;
+				Date now = new Date();
+				String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+						Locale.US).format(now);
+				// String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+				// new Locale("ar")).format(now);
+				// String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+				// Locale.getDefault()).format(now);
+
+				String path = mediaStorageDir.getPath() + File.separator;
+				if (mediaType == MEDIA_TYPE_IMAGE) {
+					mediaFile = new File(path + "IMG_" + timeStamp + ".jpg");
+				} else if (mediaType == MEDIA_TYPE_VIDEO) {
+					mediaFile = new File(path + "VID_" + timeStamp + ".mp4");
+				} else {
+					return null;
+				}
+
+				Log.d(TAG, "File: " + Uri.fromFile(mediaFile));
+
+				// 5. Return the file's URI.
+				return Uri.fromFile(mediaFile);
+			} else
+				return null;
+		}
+
+		private boolean isExternalStorageAvailable() {
+			String state = Environment.getExternalStorageState();
+
+			return state.equals(Environment.MEDIA_MOUNTED);
+
+			/*
+			 * if(state.equals(Environment.MEDIA_MOUNTED)){ return true; } else
+			 * return false;
+			 */
+		}
+	};
 
 	/**
 	 * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -31,30 +193,31 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 	/**
 	 * The {@link ViewPager} that will host the section contents.
 	 */
-	ViewPager mViewPager;	
+	ViewPager mViewPager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.activity_main);
-		
+
 		ParseAnalytics.trackAppOpened(getIntent());
 
 		ParseUser currentUser = ParseUser.getCurrentUser();
-		if(currentUser == null){
+		if (currentUser == null) {
 			navigateToLogin();
 		} else {
 			Log.i(TAG, currentUser.getUsername());
 		}
-		
+
 		// Set up the action bar.
 		final ActionBar actionBar = getActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
 		// Create the adapter that will return a fragment for each of the three
 		// primary sections of the activity.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+		mSectionsPagerAdapter = new SectionsPagerAdapter(this,
+				getSupportFragmentManager());
 
 		// Set up the ViewPager with the sections adapter.
 		mViewPager = (ViewPager) findViewById(R.id.pager);
@@ -83,6 +246,66 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 		}
 	}
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (resultCode == RESULT_OK) {
+			if(requestCode == PICK_VIDEO_PHOTO_REQUEST){
+				if(data == null){
+					Toast.makeText(this, getString(R.string.general_error), Toast.LENGTH_LONG).show();
+				}
+				else {
+					mMediaUri = data.getData();
+				}
+				
+				Log.i(TAG, "mMediaUri: " + mMediaUri);
+				//if(requestCode == PICK_VIDEO_REQUEST){
+				if(video_request_flag == true){
+					//make sure the file is less than 10 MB
+					int fileSize = 0;
+					InputStream inputStream = null;
+					
+					try{
+						inputStream = getContentResolver().openInputStream(mMediaUri);
+						fileSize = inputStream.available();
+					}
+					catch (FileNotFoundException e){
+						Toast.makeText(this, R.string.error_openeing_file, Toast.LENGTH_LONG).show();
+						return;
+					}
+					catch (IOException e){
+						Toast.makeText(this, R.string.error_openeing_file, Toast.LENGTH_LONG).show();
+						return;
+					}
+					finally{
+						try {
+							inputStream.close();
+						} catch (IOException e) {/* Intentionally blank */}
+					}
+					
+					if(fileSize >= FILE_SIZE_LIMIT){
+						Toast.makeText(this, R.string.error_file_size_too_large, Toast.LENGTH_LONG).show();
+						video_request_flag = false;
+						return;
+					}
+					video_request_flag = false;
+				}
+				
+				//}
+			}
+			else {
+				Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+				mediaScanIntent.setData(mMediaUri);
+				sendBroadcast(mediaScanIntent);
+			}
+		} else if (resultCode != RESULT_CANCELED) {
+			Toast.makeText(this, R.string.general_error, Toast.LENGTH_LONG)
+					.show();
+		}
+
+	}
+
 	private void navigateToLogin() {
 		Intent intent = new Intent(this, LoginActivity.class);
 		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -103,15 +326,23 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_logout) {
+		switch (item.getItemId()) {
+		case R.id.action_logout:
 			ParseUser.logOut();
 			navigateToLogin();
-		}
-		else if(id == R.id.action_edit_friends){
+			break;
+		case R.id.action_edit_friends:
 			Intent intent = new Intent(this, EditFriendsActivity.class);
 			startActivity(intent);
+			break;
+		case R.id.action_camera:
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setItems(R.array.camera_choices, mDialogListener);
+			AlertDialog dialog = builder.create();
+			dialog.show();
+			break;
 		}
+
 		return super.onOptionsItemSelected(item);
 	}
 
@@ -135,39 +366,32 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
 	/**
 	 * A placeholder fragment containing a simple view.
-	 *//*
-	public static class PlaceholderFragment extends Fragment {
-		*//**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 *//*
-		private static final String ARG_SECTION_NUMBER = "section_number";
-
-		*//**
-		 * Returns a new instance of this fragment for the given section number.
-		 *//*
-		public static PlaceholderFragment newInstance(int sectionNumber) {
-			PlaceholderFragment fragment = new PlaceholderFragment();
-			Bundle args = new Bundle();
-			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			TextView textView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			textView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
-			return rootView;
-		}
-	}*/
+	 */
+	/*
+	 * public static class PlaceholderFragment extends Fragment {
+	 *//**
+	 * The fragment argument representing the section number for this
+	 * fragment.
+	 */
+	/*
+	 * private static final String ARG_SECTION_NUMBER = "section_number";
+	 *//**
+	 * Returns a new instance of this fragment for the given section number.
+	 */
+	/*
+	 * public static PlaceholderFragment newInstance(int sectionNumber) {
+	 * PlaceholderFragment fragment = new PlaceholderFragment(); Bundle args =
+	 * new Bundle(); args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+	 * fragment.setArguments(args); return fragment; }
+	 * 
+	 * public PlaceholderFragment() { }
+	 * 
+	 * @Override public View onCreateView(LayoutInflater inflater, ViewGroup
+	 * container, Bundle savedInstanceState) { View rootView =
+	 * inflater.inflate(R.layout.fragment_main, container, false); TextView
+	 * textView = (TextView) rootView .findViewById(R.id.section_label);
+	 * textView.setText(Integer.toString(getArguments().getInt(
+	 * ARG_SECTION_NUMBER))); return rootView; } }
+	 */
 
 }
